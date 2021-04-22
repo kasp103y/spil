2f0190859d64d8ae63669e443daa27b310c763dc
truttedyr <kaspernikolajsendahl@gmail.com>
2021-04-22 13:41:13 +0200

    spil v3

---
+++ script.js
@@ -0,0 +1,89 @@
+let points;
+let liv;
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+
+    points = 0;
+    document.querySelector("#score_board_sprite").innerHTML = points;
+    liv = 3;
+    console.log("sidenVises");
+
+    document.querySelector("#vand_container").classList.add("pos1", "fald");
+
+    document.querySelector("#vand_container").addEventListener("mousedown", clickVandHandler);
+
+    document.querySelector("#ild_container").classList.add("pos2", "fald");
+
+    document.querySelector("#ild_container").addEventListener("mousedown", clickIldHandler);
+
+    document.querySelector("#ild_container").addEventListener("animationiteration", ildReset);
+    document.querySelector("#vand_container").addEventListener("animationiteration", vandReset);
+
+
+
+
+}
+
+function clickVandHandler() {
+    console.log("clickVandhandler");
+
+    document.querySelector("#vand_container").removeEventListener("mousedown", clickVandHandler);
+    document.querySelector("#vand_sprite").classList.add("splat_vand");
+    points++;
+    document.querySelector("#score_board_sprite").innerHTML = points;
+    document.querySelector("#vand_container").classList.add("frys");
+    document.querySelector("#vand_container").addEventListener("animationend", vandReset);
+
+}
+
+function vandReset() {
+    console.log("vandReset");
+    document.querySelector("#vand_container").classList = "";
+    document.querySelector("#vand_sprite").classList = "";
+    document.querySelector("#vand_container").removeEventListener("animationend", vandReset);
+    document.querySelector("#vand_container").offsetHeight;
+    document.querySelector("#vand_container").classList.add("pos3", "fald");
+    document.querySelector("#vand_container").addEventListener("mousedown", clickVandHandler);
+
+
+}
+
+function clickIldHandler() {
+    console.log("clickIldhandler");
+
+    document.querySelector("#ild_container").removeEventListener("mousedown", clickIldHandler);
+    document.querySelector("#ild_sprite").classList.add("skaler");
+    console.log("liv =" + liv)
+
+    document.querySelector("#liv" + liv).classList.add("hide");
+
+    liv--;
+
+    console.log("liv er nu =" + liv)
+
+    document.querySelector("#ild_container").classList.add("frys");
+    document.querySelector("#ild_container").addEventListener("animationend", ildReset);
+}
+
+function ildReset() {
+    console.log("ildReset");
+    document.querySelector("#ild_container").classList = "";
+    document.querySelector("#ild_sprite").classList = "";
+    document.querySelector("#ild_container").removeEventListener("animationend", ildReset);
+    document.querySelector("#ild_container").offsetHeight;
+    document.querySelector("#ild_container").classList.add("pos4", "fald");
+    document.querySelector("#ild_container").addEventListener("mousedown", clickIldHandler);
+
+
+}
+
+function ildForsvind() {
+    console.log("ildForsvind");
+}
+
+
+function vandForsvind() {
+    console.log("vandForsvind");
+}
